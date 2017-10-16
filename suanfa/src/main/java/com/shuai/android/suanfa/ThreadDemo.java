7cbfb1a7c01b60b08cbf879d5a292aa60ae8d11b
shuaizhimin <4402395>
2017-10-16 22:47:20 +0800

    学习提交

--- suanfa/src/main/java/com/shuai/android/suanfa/ThreadDemo.java
+++ suanfa/src/main/java/com/shuai/android/suanfa/ThreadDemo.java
@@ -7,6 +7,54 @@ package com.shuai.android.suanfa;
  * Time:下午3:46
  */
 public class ThreadDemo {
+    private int j;
+    String mString;
+    StringBuffer mStringBuffer;
+
+    public static void main(String args[]) {
+        ThreadDemo tt = new ThreadDemo();
+        Inc inc = tt.new Inc();
+        Dec dec = tt.new Dec();
+
+        Thread t1 = new Thread(inc);
+        Thread t2 = new Thread(dec);
+        Thread t3 = new Thread(inc);
+        Thread t4 = new Thread(dec);
+
+        t1.start();
+        t2.start();
+        t3.start();
+        t4.start();
+    }
 
 
+    public synchronized void inc() {
+        j++;
+        System.out.println(Thread.currentThread().getName() + " inc:" + j);
+    }
+
+    public synchronized void dec() {
+        j--;
+        System.out.println(Thread.currentThread().getName() + " dec:" + j);
+    }
+
+    class Inc implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                inc();
+            }
+        }
+    }
+
+    class Dec implements Runnable {
+
+        @Override
+        public void run() {
+            for (int i = 0; i < 100; i++) {
+                dec();
+            }
+        }
+    }
 }
